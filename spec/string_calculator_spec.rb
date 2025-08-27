e025521f69a31188481f0e7192079ea21d589e6a
Dipak_2 <dipak_2@DESKTOP-NAAJ95M.localdomain>
2025-08-27 13:34:25 +0000

    block for comma-separated numbers,multiple numbers,new lines between numbers,custom delimiters

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,11 +2,17 @@
 require_relative '../string_calculator'
 
 RSpec.describe StringCalculator do
-   it "returns 0 for empty string" do
-      expect(StringCalculator.add("")).to eq(0)
+    it "returns 0 for empty string" do
+        expect(StringCalculator.add("")).to eq(0)
+        end
+
+    it "returns the number itself when only one number" do
+        expect(StringCalculator.add("1")).to eq(1)
     end
 
-  it "returns the number itself when only one number" do
-    expect(StringCalculator.add("1")).to eq(1)
-  end
+    it "returns the sum of two comma-separated numbers" do
+        expect(StringCalculator.add("1,5")).to eq(6)
+    end
+
+
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -8,9 +8,17 @@ class StringCalculator
       return string_numbers.to_i
     end
 
+   # Check for custom delimiter
+    if string_numbers.start_with?("//")
+      delimiter, string_numbers = string_numbers[2..].split("\n", 2)
+      numbers_array = string_numbers.split(delimiter).map(&:to_i)
+    else
+      # Split by comma or newline
+      numbers_array = string_numbers.split(/,|\n/).map(&:to_i)
+    end
+   
 
-    # For now, just handle a single number or empty string
-    string_numbers.split(',').map(&:to_i).sum
+      numbers_array.sum
   end
 end
 
